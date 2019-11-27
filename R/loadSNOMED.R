3d1508fffa91c6c02701783fb0c5289979af19f2
Anoop Shah <a.shah@ucl.ac.uk>
2019-11-27 11:04:06 +0000

    Load SNOMED function created and tested on basic data.

--- R/loadSNOMED.R
+++ R/loadSNOMED.R
@@ -14,15 +14,12 @@
 #'
 #' @import data.table
 #' @import bit64
-loadSNOMED <- function(folderpaths, active_only = TRUE){
+loadSNOMED <- function(folders, active_only = TRUE){
 	# Returns an environment containing the SNOMED CT files
 	# loaded into data.tables
-	
+	SNOMED <- new.env()
 	append <- FALSE
-	for (folderpath in folderpaths){
-# assign('NOTE', paste0('SNOMED CT files loaded from ', folderpath),
-#	envir = SNOMED)
-
+	for (folder in folders){
 		files <- dir(folder)
 		used <- rep(FALSE, length(files))
 		for (filename in c('Concept', 'Description', 'StatedRelationship',
@@ -30,44 +27,75 @@ loadSNOMED <- function(folderpaths, active_only = TRUE){
 			touse <- which(files %like% filename & used == FALSE)
 			used[touse] <- TRUE
 			if (length(touse == 1)){
-				TEMP <- data.table::fread(paste0(folder, '/', files[touse]),
-					quote = "")
-				# Convert all 'time' columns to times (YYYYMMDD format)
-				toconvert <- names(TEMP)[names(TEMP) %like% 'Time']
-				if (length(toconvert) > 0){
-					for (i in toconvert){
-						try(TEMP[, .temp := data.table::as.IDate(
-							as.character(get(i)), '%Y%m%d')])
-						if ('.temp' %in% names(TEMP)){
+				message('Attempting to load ', files[touse])
+				TEMP <- NULL
+				try(TEMP <- data.table::fread(paste0(folder, '/',
+					files[touse]), quote = ""))
+				if (is.null(TEMP)){
+					warning('Failed to load file.')
+				} else {
+					message('  Loaded ', nrow(TEMP), ' rows.')
+					# Convert all 'time' columns to times (YYYYMMDD format)
+					toconvert <- names(TEMP)[names(TEMP) %like% 'Time']
+					if (length(toconvert) > 0){
+						for (i in toconvert){
+							message('  Converting ', i, ' to IDate.')
+							try(TEMP[, .temp := data.table::as.IDate(
+								as.character(get(i)), '%Y%m%d')])
+							if ('.temp' %in% names(TEMP)){
+								TEMP[, (i) := NULL]
+								data.table::setnames(TEMP, '.temp', i)
+							}
+						}
+					}
+					# Convert all integer 'Id' columns to integer64
+					toconvert <- names(TEMP)[which(
+						(names(TEMP) %like% 'Id$') &
+						(sapply(TEMP, class) == 'integer'))]
+					if (length(toconvert) > 0){
+						for (i in toconvert){
+							message('  Converting ', i, ' to integer64.')
+							TEMP[, .temp := bit64::as.integer64(get(i))]
 							TEMP[, (i) := NULL]
 							data.table::setnames(TEMP, '.temp', i)
 						}
 					}
-				}
-				# Convert all integer columns to integer64
-				toconvert <- names(TEMP)[
-					which(sapply(TEMP, class) == 'integer')]
-				if (length(toconvert) > 0){
-					for (i in toconvert){
-						TEMP[, .temp := bit64::as.integer64(get(i))]
-						TEMP[, (i) := NULL]
-						data.table::setnames(TEMP, '.temp', i)
+					# Convert 'active' columns to logical
+					if ('active' %in% names(TEMP)){
+						if (active_only){
+							message('  Limiting to active rows (', 
+								sum(TEMP$active), '/', nrow(TEMP), ').')
+							TEMP <- TEMP[active == TRUE]
+							TEMP[, active := NULL]
+						} else if (
+							all(as.integer64(SNOMED$RELATIONSHIP$active))
+							%in% as.integer64(c(0, 1))){
+							message('  Converting active to logical.')
+							TEMP[, .temp := as.logical(active)]
+							TEMP[, active := NULL]
+							data.table::setnames(TEMP, '.temp', 'active')
+						}
 					}
+					if (append){
+						message('  Appending to ', toupper(filename))
+						TEMP2 <- get(toupper(filename), envir = SNOMED)
+						try(TEMP <- rbind(TEMP, TEMP2, use.names = TRUE))
+					} else {
+						message('  Naming as ', toupper(filename))
+					}
+					assign(toupper(filename), value = TEMP, envir = SNOMED)
 				}
-				cat('\nLoaded', files[touse], '(', nrow(TEMP), 'rows).\n')
-				if (append){
-					TEMP2 <- get(toupper(filename), envir = SNOMED)
-					try(TEMP <- rbind(TEMP, TEMP2, use.names = TRUE))
-				}
-				assign(toupper(filename), value = TEMP, envir = myenv)
 			} else {
-				warning('Unable to load', touse)
+				warning('Unable to identify correct file from ', touse)
 			}
 		}
 		
 		# Append files from 2nd and subsequent folders
 		append <- TRUE
 	}
+	
+	cat('\nSNOMED CT tables loaded into environment:\n')
+	tables(env = SNOMED)
 	return(SNOMED)
 }
 
