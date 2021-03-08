5523f7f92912d1c8841408eb38431f42334a4e6f
Anoop Shah <a.shah@ucl.ac.uk>
2021-03-08 17:21:35 +0000

    Tidying up the documentation for data tables

--- R/RELATIONSHIP.R
+++ R/RELATIONSHIP.R
@@ -16,17 +16,17 @@
 #'
 #' @details
 #' \itemize{
-#'   \item{id}{integer64: ID of the relationship record (primary key)} 
-#'   \item{active}{integer: whether this concept is currently active}
-#'   \item{moduleId}{integer64: class of SNOMED CT concept (whether it
+#'   \item{id}{ integer64: ID of the relationship record (primary key)} 
+#'   \item{active}{ integer: whether this concept is currently active}
+#'   \item{moduleId}{ integer64: class of SNOMED CT concept (whether it
 #'     is used for recording information or is a metadata concept)}
-#'   \item{sourceId}{integer64: source SNOMED CT concept for the relationship}
-#'   \item{destinationId}{integer64: destination SNOMED CT concept for the relationship}
-#'   \item{relationshipGroup}{integer: group ID for relationships that are grouped}
-#'   \item{characteristicTypeId}{integer64: 900000000000011006 = Inferred relationship}
-#'   \item{modifierId}{integer64: 900000000000451002 = Existential restriction modifier}
-#'   \item{effectiveTime}{IDate: when the concept became active}
-#'   \item{typeId}{integer64: type of relationship, e.g.
+#'   \item{sourceId}{ integer64: source SNOMED CT concept for the relationship}
+#'   \item{destinationId}{ integer64: destination SNOMED CT concept for the relationship}
+#'   \item{relationshipGroup}{ integer: group ID for relationships that are grouped}
+#'   \item{characteristicTypeId}{ integer64: 900000000000011006 = Inferred relationship}
+#'   \item{modifierId}{ integer64: 900000000000451002 = Existential restriction modifier}
+#'   \item{effectiveTime}{ IDate: when the concept became active}
+#'   \item{typeId}{ integer64: type of relationship, e.g.
 #'     116680003 = Is a, 42752001 = Due to, 246090004 = Associated finding,
 #'     363698007 = Finding site, 363702006 = Has focus}
 #' }
