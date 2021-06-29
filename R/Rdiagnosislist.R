27119870e30738f32dd74f374e4fe259f612662a
Anoop <anoop@doctors.org.uk>
2021-06-29 17:24:16 +0100

    Ready to release to CRAN

--- R/Rdiagnosislist.R
+++ R/Rdiagnosislist.R
@@ -1,8 +1,8 @@
 #' Rdiagnosislist: A package for manipulating SNOMED CT diagnosis lists
 #'
 #' The Rdiagnosislist package makes it easy to load a SNOMED dictionary
-#' into R and use the hierarchies to search for terms and navigate
-#' relations between terms.
+#' into R and use the hierarchies to search for concepts and navigate
+#' relations between concepts.
 #'
 #' @docType package
 #' @name Rdiagnosislist
---
+++ cran-comments.md
@@ -0,0 +1,11 @@
+## Test environments
+* local Ubuntu 18.04, R 3.4.4
+* win-builder (devel and release)
+
+## R CMD check results
+Status: 1 NOTE
+
+* checking CRAN incoming feasibility ... NOTE
+Maintainer: ‘Anoop Shah <anoop@doctors.org.uk>’
+
+New submission
