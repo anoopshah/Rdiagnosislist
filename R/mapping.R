835d0863ee0644810c09234e5b7c0184492d6046
Anoop <anoop@doctors.org.uk>
2021-07-19 16:21:01 +0100

    Working on mapping functions

--- R/mapping.R
+++ R/mapping.R
@@ -12,32 +12,41 @@
 #' derived from the NHS Digital 'Data Migration' pack. The
 #' mapping file must be a data.table object with columns:
 #' conceptId (integer64, unique),
-#' v2_code (character list of 7-character Read V2 codes),
-#' v2_term (character list of Read V2 terms),
-#' v3_concept (character list of CTV3 concept codes),
-#' v3_termid (character list of CTV3 term description codes)
+#' read2_code (character list of 7-character Read V2 codes),
+#' read2_term (character list of Read V2 terms),
+#' ctv3_concept (character list of CTV3 concept codes),
+#' ctv3_termid (character list of CTV3 term description codes)
 #'
 #' @param x SNOMEDcodelist or SNOMEDconcept object
-#' @param mappingtable data.table containing mapping file
-#'
-#' @return a data.table with the following columns: conceptId,
-#'   mapped 
+#' @param mappingtable data.table containing mapping file, which must 
+#'   contain a unique field 'conceptId' and fields named 'read2_code'
+#'   and 'read2_term' (for mapping to Read 2) or 'ctv3_concept'
+#'   and 'ctv3_termid' (for mapping to Clinical Terms Version 3).
+#' @return a data.table containing the columns conceptId and . There may be multiple 
+#'   rows per conceptId
 #' @export
 #' @seealso MAPS
 #' @examples
 #' # Load sample SNOMED CT dictionary
 #' SNOMED <- sampleSNOMED()
+#' data(MAPS)
 #' 
 #' # Example: Mapping a single concept
-#' getMaps(SNOMEDconcept('Heart failure'))
+#' getMaps(SNOMEDconcept('Heart failure'), mappingtable = MAPS, to = 'read')
 #' # Example: Mapping a concept and its descendants
-#' getMaps(descendants(SNOMEDconcept('Heart failure')))
-getMaps <- function(conceptIds, mappingtable, 
+#' getMaps(descendants(SNOMEDconcept('Heart failure')), mappingtable = MAPS, to = 'read')
+#' # Example: Mapping a codelist
+#' getMaps()
+getMaps <- function(x, mappingtable, to = c('read2', 'ctv3'),
 	SNOMED = getSNOMED()){
-	# Returns the original concepts and the linked concepts
+	# Returns the original concepts and the linked concepts as a
+	# data.table
+	MAPS <- mapping
 	if (is.SNOMEDconcept(x)){
-		mappingtable[data.table(conceptId = x), on = 'conceptId']
+		MAPPED <- mappingtable[data.table(conceptId = x), on = 'conceptId']
+		MAPPED
 	} else if (is.SNOMEDcodelist(x)){
-		merge(mappingtable[x, on = 'conceptId'])
+		MAPPED <- merge(mappingtable[x, on = 'conceptId'])
 	}
 }
+
